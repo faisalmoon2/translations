2af509212462de10a9feced0d42b6e3a883391df
Faisal Nadeem <47865462+faisalmoon2@users.noreply.github.com>
2022-09-13 14:33:20 +0300

    Update Cache.php

--- Language/ur/Cache.php
+++ Language/ur/Cache.php
@@ -1,1 +1,18 @@
+<?php
 
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Cache language settings
+return [
+    'unableToWrite'   => 'کیش {0} پر لکھنے سے قاصر ہے',
+    'invalidHandlers' => 'کیشے کی ترتیبات میں $validHandlers کی ایک صف شامل ہونی چاہیے۔',
+    'noBackup'        => 'کیشے کی ترتیبات میں ہینڈلر اور بیک اپ ہینڈلر ہونا ضروری ہے۔',
+    'handlerNotFound' => 'کیش سیٹنگز میں غلط ہینڈلر یا بیک اپ ہینڈلر شامل ہے۔',
+];
